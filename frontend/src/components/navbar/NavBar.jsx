a9af8bcb3a78db4fce1e8a4f95360aac5441f032
ahamed suldhansha <iahamedsha@gmail.com>
2025-01-19 07:40:40 +0530

    feat: added initials in crest

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -9,7 +9,10 @@ function NavBar() {
       <div className="nav-bar">
         <div className="nav-bar-heading">
           <h1>D.R Yasu</h1>
-          <img className="nav-crest" src={crestUrl} alt="YB" />
+          <div className="nav-crest-container">
+            <img className="nav-crest" src={crestUrl} alt="YB" />
+
+          </div>
           <h1>Bharathi</h1>
         </div>
       </div>

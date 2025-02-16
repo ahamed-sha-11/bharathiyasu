f5580d7e1f2c39572c16fa6d97052575e4c3907b
Maddy6914 <madmadhan661@gmail.com>
2025-02-17 02:10:45 +0530

    refactor: DIR sir-portfilo

---
+++ backend/sir-portfilo/src/test/java/com/sir_portfilo/SirPortfiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sir_portfilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirPortfiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

322829ad18a068a8300b7396577954938776a936
ahamed suldhansha <iahamedsha@gmail.com>
2025-04-14 00:41:41 +0530

    chore: renamed db keys

--- backend/configs/supabase.js
+++ backend/configs/supabase.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
